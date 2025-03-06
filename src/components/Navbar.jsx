2d835897577c41f82632d8612223e1fde447b929
user <user@example.com>
2025-03-06 05:58:10 +0530

    Add Profile Details

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,17 +2,17 @@ import React from "react";
 import ProfileInfo from "./ProfileInfo";
 import { useNavigate } from "react-router-dom";
 
-const navigate = useNavigate;
-
-const Navbar = () => {
+const Navbar = ({ userInfo }) => {
+  const navigate = useNavigate();
   const handleLogout = () => {
+    localStorage.clear();
     navigate("/login");
   };
   return (
     <div className="bg-white flex items-center justify-between px-6 py-2 drop-shadow">
       <h2 className="text-xl font-medium text-black py-2">Notes</h2>
 
-      <ProfileInfo onLogout={handleLogout} />
+      <ProfileInfo userInfo={userInfo} onLogout={handleLogout} />
     </div>
   );
 };
--- src/components/ProfileInfo.jsx
+++ src/components/ProfileInfo.jsx
@@ -1,10 +1,10 @@
 import React from "react";
 
-const ProfileInfo = ({ onLogout }) => {
+const ProfileInfo = ({ userInfo, onLogout }) => {
   return (
     <div>
       <div className="">
-        <p className="text-sm font-medium">John</p>
+        <p className="text-sm font-medium">{userInfo?.fullName}</p>
         <button className="" onClick={onLogout}>
           Logout
         </button>
